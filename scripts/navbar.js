a879f90ad680549bde2aed386e9cc45a5cc5adaf
petermendoza <peterjohnm2002@gmail.com>
2023-09-07 20:58:31 -0500

    about section done, just need icon stuff, projects, and experience left

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,6 +1,13 @@
-let nav = 0;
-
-function changeActive(element){
+function changeActive(line, text){
+    var navLines = document.getElementsByClassName("nav-lines");
+    var navTicks = document.getElementsByClassName("nav-tick");
     
-document.getElementsByClassName("nav-lines").style.color = "#FFFFFF"
+    for (var i = 0; i < navLines.length; i++) {
+        navLines[i].style.color = "#949494";
+        navTicks[i].style.cssText = "width: 10px; background-color:#949494; border: #949494 solid 1px;"
+    }
+    document.getElementById(text).style.color = "#cdd5f1";
+    document.getElementById(line).style.cssText = "width: 35px; border: #cdd5f1 solid 1px; background-color:#cdd5f1";
+
+
 }
