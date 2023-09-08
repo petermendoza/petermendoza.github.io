c881a759374618788b3fd87f1cea286cc1b1a309
petermendoza <peterjohnm2002@gmail.com>
2023-09-08 16:34:05 -0500

    added transitions for nav and made it functional

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,13 +1,22 @@
+let nav;
 function changeActive(line, text){
     var navLines = document.getElementsByClassName("nav-lines");
     var navTicks = document.getElementsByClassName("nav-tick");
-    
     for (var i = 0; i < navLines.length; i++) {
         navLines[i].style.color = "#949494";
         navTicks[i].style.cssText = "width: 10px; background-color:#949494; border: #949494 solid 1px;"
     }
+    document.getElementById("about-content").style.opacity = "0%";
+    document.getElementById("projects-content").style.opacity = "0%";
+    document.getElementById("experience-content").style.opacity = "0%";
+    if(nav != text){
     document.getElementById(text).style.color = "#cdd5f1";
     document.getElementById(line).style.cssText = "width: 35px; border: #cdd5f1 solid 1px; background-color:#cdd5f1";
-
+    document.getElementById(text.replace('nav','content')).style.opacity = "100%";
+    nav = text;
+    }
+    else{
+        nav = 0;
+    }
 
 }
